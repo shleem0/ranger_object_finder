27f51a461f34a948b2441cea7949c36ca2f1da95
Sholto <sholtokinghorn@gmail.com>
2025-01-28 16:17:08 +0000

    ros ws set up

---
+++ ros_ws/build/openslam_gmapping/scanmatcher/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /afs/inf.ed.ac.uk/user/s22/s2281597/Ranger-object-finder/ros_ws/src/slam_gmapping/openslam_gmapping/scanmatcher
+# Build directory: /afs/inf.ed.ac.uk/user/s22/s2281597/Ranger-object-finder/ros_ws/build/openslam_gmapping/scanmatcher
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ros_ws/build/slam_gmapping/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/afs/inf.ed.ac.uk/user/s22/s2281597/Ranger-object-finder/ros_ws/install/openslam_gmapping/share/openslam_gmapping/package.sh"
