6c22d9497ff3233b0e57930529a14f45507467bd
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-01-30 16:15:08 +0000

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Ranger development flake";
+  inputs.nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+  inputs.nixpkgs.follows = "nix-ros-overlay/nixpkgs";
+
+  outputs = { nixpkgs, nix-ros-overlay, ... }:
+    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nix-ros-overlay.overlays.default ];
+        };
+        compile-packages = with pkgs; [
+          cabal-install
+          ghc
+        ];
+        ros-packages = with pkgs; [
+          colcon
+          (with rosPackages.humble; buildEnv {
+            paths = [ ros-core ];
+          })];
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "Ranger compile shell";
+          packages = compile-packages ++ ros-packages;
+        };
+    });
+}
