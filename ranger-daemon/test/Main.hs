ef7e577c945b1028775d8bf287c514c441ec5345
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-02-03 18:56:15 +0000

    Create tests for calling C functions

---
+++ ranger-daemon/test/Main.hs
@@ -0,0 +1,20 @@
+module Main (main) where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import Ranger.Bluetooth.SimpleBLE.Raw
+
+main :: IO ()
+main = defaultMain $
+  testGroup "Ranger.Bluetooth.SimpleBLE"
+    [ testGroup "Raw" [ callAdapterIsBluetoothEnabled ] ]
+
+callAdapterGetCount :: TestTree
+callAdapterGetCount = testCase "simpleble_adapter_get_count" $ do
+  c <- simpleble_adapter_get_count
+  print c
+
+callAdapterIsBluetoothEnabled :: TestTree
+callAdapterIsBluetoothEnabled = testCase "simpleble_adapter_is_bluetooth_enabled" $ do
+  b <- simpleble_adapter_is_bluetooth_enabled
+  print b
