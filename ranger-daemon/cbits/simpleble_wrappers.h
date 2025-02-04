740756d4591d206d100f7661d3214cf21f410156
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-02-04 18:33:18 +0000

    Remove some wrappers

--- ranger-daemon/cbits/simpleble_wrappers.h
+++ ranger-daemon/cbits/simpleble_wrappers.h
@@ -1,7 +1,3 @@
 #pragma once
 
 #include <simpleble_c/simpleble.h>
-
-size_t adapter_get_count();
-
-simpleble_err_t test_err();
--- ranger-daemon/src/Ranger/Bluetooth/SimpleBLE/Raw.hs
+++ ranger-daemon/src/Ranger/Bluetooth/SimpleBLE/Raw.hs
@@ -3,8 +3,8 @@ module Ranger.Bluetooth.SimpleBLE.Raw where
 import Ranger.Bluetooth.SimpleBLE.Raw.Types
 import Foreign.C
 
-foreign import ccall unsafe "adapter_get_count"
+foreign import ccall unsafe "simpleble_c/simpleble.h simpleble_adapter_get_count"
   simpleble_adapter_get_count :: IO CSize
 
-foreign import ccall unsafe "adapter_is_bluetooth_enabled"
+foreign import ccall unsafe "simpleble_c/simpleble.h simpleble_adapter_is_bluetooth_enabled"
   simpleble_adapter_is_bluetooth_enabled :: IO CBool
