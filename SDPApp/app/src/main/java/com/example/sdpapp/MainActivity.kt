557acd55e02c0adeb07eaefaa6248cbd6309552c
ryndubei <114586905+ryndubei@users.noreply.github.com>
2025-02-11 14:51:06 +0000

    Create ranger bluetooth service for app

--- SDPApp/app/src/main/java/com/example/sdpapp/MainActivity.kt
+++ SDPApp/app/src/main/java/com/example/sdpapp/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.sdpapp
 
+import android.content.BroadcastReceiver
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.ServiceConnection
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.IBinder
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +17,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.sdpapp.bt.RangerBluetoothService
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sdpapp.ui.BottomNavigationBar
 import com.example.sdpapp.ui.ThemeViewModelFactory
@@ -17,6 +25,65 @@ import com.example.sdpapp.ui.theme.SDPAppTheme
 import com.example.sdpapp.ui.theme.ThemeViewModel
 
 class MainActivity : ComponentActivity() {
+    private var bluetoothService : RangerBluetoothService? = null
+
+    private val serviceConnection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(cn: ComponentName?, bnd: IBinder?) {
+            bluetoothService = (bnd as RangerBluetoothService.LocalBinder).getService()
+            bluetoothService?.let { bluetooth ->
+                if (!bluetooth.initialize()) {
+                    Log.e("MainActivity", "Unable to initialize Bluetooth")
+                    // may want to show the user a failure message instead of this
+                    finish()
+                }
+                bluetooth.connectForDemo()
+            }
+        }
+
+        override fun onServiceDisconnected(cn: ComponentName?) {
+            bluetoothService = null
+        }
+    }
+
+    private val gattUpdateReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(ctx: Context, it: Intent) {
+            when (intent.action) {
+                RangerBluetoothService.Companion.ACTION_GATT_CONNECTED -> {
+                    Log.i("MainActivity", "Connected to GATT device")
+                }
+                RangerBluetoothService.Companion.ACTION_GATT_DISCONNECTED -> {
+                    Log.w("MainActivity", "Disconnected from GATT device")
+                }
+                RangerBluetoothService.Companion.ACTION_GATT_READY -> {
+                    Log.i("MainActivity", "GATT device ready for startDemo command")
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerReceiver(gattUpdateReceiver, makeGattUpdateIntentFilter())
+        val s = bluetoothService
+        if (s != null) {
+            val result = s.connectForDemo()
+            Log.d("MainActivity", "Connect request result=$result")
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(gattUpdateReceiver)
+    }
+
+    private fun makeGattUpdateIntentFilter(): IntentFilter {
+        return IntentFilter().apply {
+            addAction(RangerBluetoothService.Companion.ACTION_GATT_CONNECTED)
+            addAction(RangerBluetoothService.Companion.ACTION_GATT_DISCONNECTED)
+            addAction(RangerBluetoothService.Companion.ACTION_GATT_READY)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -27,6 +94,8 @@ class MainActivity : ComponentActivity() {
                 BottomNavigationBar(themeViewModel)
             }
         }
+        val gattServiceIntent = Intent(this, RangerBluetoothService::class.java)
+        bindService(gattServiceIntent, serviceConnection, Context.BIND_AUTO_CREATE)
     }
 
 
