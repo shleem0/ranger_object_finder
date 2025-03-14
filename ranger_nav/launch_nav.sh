6397cc666a3a739d31a7d729faf5abc076c1de75
shleem0 <sholtokinghorn@gmail.com>
2025-03-14 16:11:36 +0000

    adjusted launch script

--- ranger_nav/launch_nav.sh
+++ ranger_nav/launch_nav.sh
@@ -1,6 +1,6 @@
 ros2 run slam_toolbox async_slam_toolbox_node --ros-args -p odom_frame:=odom --ros-args -p base_frame:=base_link --ros-args -p map_frame:=map --ros-args -p scan_topic:=/base_scan --ros-args -p map_update_interval:=0.1 --ros-args -p max_laser_range:=3.5 --ros-args -p minimum_travel_distance:=0.1 --ros-args -p use_scan_matching:=true --ros-args -p minimum_travel_heading:=1.57 --ros-args -p do_loop_closing:=true &
 ros2 launch nav2_bringup navigation_launch.py &
-ros2 launch hls_lfcd_lds_driver hlds_laser.launch.py &
+ros2 launch ld08_driver ld08.launch.py &
 ros2 run movement_controller odom_publisher & 
 sudo python3 motor/encoder.py & 
 sudo python3 motor/motor_data.py
