cd03e3a2b7d9564fd6054b065be284b447ab4dd2
shleem0 <sholtokinghorn@gmail.com>
2025-03-17 15:24:44 +0000

    changed times to msgs

--- ranger_nav/ros_ws/src/movement_controller/movement_controller/odom_publisher.py
+++ ranger_nav/ros_ws/src/movement_controller/movement_controller/odom_publisher.py
@@ -83,7 +83,7 @@ class OdometryPublisher(Node):
         self.prev_motor_pos2 = motor_pos2
 
         odom = Odometry()
-        odom.header.stamp = current_time
+        odom.header.stamp = current_time.to_msg()
         odom.header.frame_id = 'odom'
         odom.child_frame_id = 'base_link'
         odom.pose.pose.position.x = self.x
@@ -102,7 +102,7 @@ class OdometryPublisher(Node):
 
         # Transform from odom to base_link
         t_odom_base = TransformStamped()
-        t_odom_base.header.stamp = current_time
+        t_odom_base.header.stamp = current_time.to_msg()
         t_odom_base.header.frame_id = 'odom'
         t_odom_base.child_frame_id = 'base_link'
 
@@ -117,7 +117,7 @@ class OdometryPublisher(Node):
 
         # Transform from base_link to base_scan
         t_base_scan = TransformStamped()
-        t_base_scan.header.stamp = current_time
+        t_base_scan.header.stamp = current_time.to_msg()
         t_base_scan.header.frame_id = 'base_link'
         t_base_scan.child_frame_id = 'laser'
 
