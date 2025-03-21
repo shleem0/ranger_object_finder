f24fc020dd4c102b96a86b7a73aeb97cd1cdae90
shleem0 <sholtokinghorn@gmail.com>
2025-03-21 18:40:49 +0000

    undid

--- ranger_nav/motor/motor_code.py
+++ ranger_nav/motor/motor_code.py
@@ -11,29 +11,31 @@ def control_motors():
         f1.write("True 0")
         f2.write("True 0")
 
-    with open("/home/ubuntu/ranger_object_finder/ranger_nav/motor/motor_input1.txt", "r") as f1, open("/home/ubuntu/ranger_object_finder/ranger_nav/motor/motor_input2.txt", "r") as f2:
+    while True:
 
-        while True:
-            
+        with open("/home/ubuntu/ranger_object_finder/ranger_nav/motor/motor_input1.txt", "r") as f1, open("/home/ubuntu/ranger_object_finder/ranger_nav/motor/motor_input2.txt", "r") as f2:
+        
             file1 = f1.read().split()
             file2 = f2.read().split()
 
-            try:
-                left_dir = eval(file1[0])
-                right_dir = eval(file2[0])
-            except:
-                left_dir = True
-                right_dir = True
+        try:
+            left_dir = eval(file1[0])
+            right_dir = eval(file2[0])
+        except:
+            left_dir = True
+            right_dir = True
 
-            try:
-                left_v = int(file1[1])
-                right_v = int(file2[1])
-            except:
-                left_v = 0
-                right_v = 0
+        try:
+            left_v = int(file1[1])
+            right_v = int(file2[1])
+        except:
+            left_v = 0
+            right_v = 0
 
-            motor.set_dir(left_dir, right_dir)
-            motor.set_speed(left_v, right_v)
+        motor.set_dir(left_dir, right_dir)
+        motor.set_speed(left_v, right_v)
+
+        time.sleep(0.1)
 
 control_motors()
 
