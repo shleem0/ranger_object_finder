f19638a20bc41d7e8960a6503a9857ee13eb5794
shleem0 <sholtokinghorn@gmail.com>
2025-03-21 18:37:49 +0000

    updated motor code

--- ranger_nav/motor/encoder.py
+++ ranger_nav/motor/encoder.py
@@ -10,18 +10,18 @@ encoder1 = GroveOpticalRotaryEncoder(PIN1)
 encoder2 = GroveOpticalRotaryEncoder(PIN2)
 
 def track_position():
-    while True:
-        with open("/home/ubuntu/ranger_object_finder/ranger_nav/motor/motor_data1.txt","w") as f1, open("/home/ubuntu/ranger_object_finder/ranger_nav/motor/motor_data1.txt","w") as f2:
-            position1 = encoder1.position()
-            f1.seek(0)      
-            f1.write(f"{position1}")
-            f1.flush()
+    with open("/home/ubuntu/ranger_object_finder/ranger_nav/motor/motor_data1.txt","w") as f1, open("/home/ubuntu/ranger_object_finder/ranger_nav/motor/motor_data1.txt","w") as f2:
+        while True:
+                position1 = encoder1.position()
+                f1.seek(0)      
+                f1.write(f"{position1}")
+                f1.flush()
 
-            position2 = encoder2.position()
-            f2.seek(0)
-            f2.write(f"{position2}")
-            f2.flush()
+                position2 = encoder2.position()
+                f2.seek(0)
+                f2.write(f"{position2}")
+                f2.flush()
 
-            time.sleep(0.1)
+                time.sleep(0.1)
 
 track_position()
--- ranger_nav/motor/motor_code.py
+++ ranger_nav/motor/motor_code.py
@@ -11,31 +11,29 @@ def control_motors():
         f1.write("True 0")
         f2.write("True 0")
 
-    while True:
+    with open("/home/ubuntu/ranger_object_finder/ranger_nav/motor/motor_input1.txt", "r") as f1, open("/home/ubuntu/ranger_object_finder/ranger_nav/motor/motor_input2.txt", "r") as f2:
 
-        with open("/home/ubuntu/ranger_object_finder/ranger_nav/motor/motor_input1.txt", "r") as f1, open("/home/ubuntu/ranger_object_finder/ranger_nav/motor/motor_input2.txt", "r") as f2:
-        
+        while True:
+            
             file1 = f1.read().split()
             file2 = f2.read().split()
 
-        try:
-            left_dir = eval(file1[0])
-            right_dir = eval(file2[0])
-        except:
-            left_dir = True
-            right_dir = True
+            try:
+                left_dir = eval(file1[0])
+                right_dir = eval(file2[0])
+            except:
+                left_dir = True
+                right_dir = True
 
-        try:
-            left_v = int(file1[1])
-            right_v = int(file2[1])
-        except:
-            left_v = 0
-            right_v = 0
+            try:
+                left_v = int(file1[1])
+                right_v = int(file2[1])
+            except:
+                left_v = 0
+                right_v = 0
 
-        motor.set_dir(left_dir, right_dir)
-        motor.set_speed(left_v, right_v)
-
-        time.sleep(0.1)
+            motor.set_dir(left_dir, right_dir)
+            motor.set_speed(left_v, right_v)
 
 control_motors()
 
