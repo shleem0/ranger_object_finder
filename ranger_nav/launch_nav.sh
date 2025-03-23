176e3dc9a98ba98897381c26d46d64b43c707882
shleem0 <sholtokinghorn@gmail.com>
2025-03-23 14:32:00 +0000

    Added map origin printing and static transform

--- ranger_nav/launch_nav.sh
+++ ranger_nav/launch_nav.sh
@@ -1,6 +1,7 @@
 ros2 run movement_controller odom_publisher & 
 ros2 launch slam_toolbox online_async_launch.py &>/dev/null &
 ros2 launch nav2_bringup navigation_launch.py &>/dev/null &
+ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 1 map odom &>/dev/null &
 ros2 launch ld08_driver ld08.launch.py &
 sudo python3 motor/encoder.py & 
 sudo python3 motor/motor_code.py
