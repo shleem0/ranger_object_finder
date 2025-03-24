89b73539079a08214547d591b4ff2774dcf73a1f
Kian3 <Kian@kiandehmahdi.co.uk>
2025-03-24 19:38:07 +0000

    changing to ascii

--- ranger_object_recognition/arduino_connection.py
+++ ranger_object_recognition/arduino_connection.py
@@ -30,7 +30,7 @@ try:
 
     # Read and print response (optional)
     while arduino.in_waiting:
-        print(arduino.readline().decode('utf-8', errors='replace').strip())
+        print(arduino.readline().decode('ascii', errors='replace').strip())
 
     # Close connection
     # arduino.close()
