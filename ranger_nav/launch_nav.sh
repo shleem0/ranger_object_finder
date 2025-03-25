05de07c26afd26b4bdd70f6843da8b09282b5f54
shleem0 <sholtokinghorn@gmail.com>
2025-03-25 20:41:48 +0000

    changed to absolute paths

--- ranger_nav/launch_nav.sh
+++ ranger_nav/launch_nav.sh
@@ -2,5 +2,5 @@ ros2 run movement_controller odom_publisher &
 ros2 launch slam_toolbox online_async_launch.py &>/dev/null &
 ros2 launch nav2_bringup navigation_launch.py &>/dev/null &
 ros2 launch ld08_driver ld08.launch.py &
-sudo python3 motor/encoder.py & 
-sudo python3 motor/motor_code.py
+sudo python3 /home/ubuntu/ranger_object_finder/ranger_nav/motor/encoder.py & 
+sudo python3 /home/ubuntu/ranger_object_finder/ranger_nav/motor/motor_code.py
