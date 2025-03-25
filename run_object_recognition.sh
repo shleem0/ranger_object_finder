def57a128b5f41c1722f0dccc9f3c8ec720bba70
Kian3 <Kian@kiandehmahdi.co.uk>
2025-03-25 20:38:09 +0000

    Update run_object_recognition.sh

--- run_object_recognition.sh
+++ run_object_recognition.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+export PYTHONPATH="/Volumes/Kian_T7/ranger_object_finder"
 python3 -m ranger_object_recognition.queue_worker --queue "ranger_object_recognition/image_queue" --interval 10 --max_files 10
