8a2afa9b3fc6b58e7251bdb3d53f7f9f9d5b36df
shleem0 <sholtokinghorn@gmail.com>
2025-03-31 12:54:10 +0100

    debugging

--- ranger_nav/imu.py
+++ ranger_nav/imu.py
@@ -2,16 +2,17 @@ import smbus
 import time
 
 AS5600_ADDR = 0x36
-AS5600_ANG = 0x0C
-
+AS5600_ANG_LSB = 0x0C
+AS5600_ANG_MSB = 0x0D
 bus = smbus.SMBus(1)
 
 def read_angle():
 
     while True:
-        raw_data = bus.read_i2c_block_data(AS5600_ADDR, AS5600_ANG, 2)
-        print(raw_data)
-        angle = (raw_data[0] << 8) + raw_data[1]
+        raw_lsb = bus.read_byte_data(AS5600_ADDR, AS5600_ANG_LSB)
+        raw_msb = bus.read_byte_data(AS5600_ADDR, AS5600_ANG_LSB)
+
+        angle = (raw_msb << 8) | raw_lsb
         print(angle)
 
         angle_deg = (angle / 4096) * 360
