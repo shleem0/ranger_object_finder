9c2ff051c459ed2dc14a4c442ee77774f067f251
shleem0 <sholtokinghorn@gmail.com>
2025-03-31 15:11:07 +0100

    dfgrf

--- ranger_nav/imu.py
+++ ranger_nav/imu.py
@@ -1,16 +1,21 @@
 from machine import I2C
-import lsm6ds3 import LSM6DS3, NORMAL_MODE_104HZ
+from LSM6DS3 import *
 import time
 
 i2c = I2C(0, scl=13, sda=12)
-sensor = LSM6DS3(i2c, mode=NORMAL_MODE_104HZ)
-
+sensor = LSM6DS3(ACC_ODR=ACC_ODR_1_66_KHz,
+                 GYRO_ODR=GYRO_ODR_1_66_KHS,
+                 enable_acc=ENABLE_ACC_ALL_AXIS,
+                 enable_gyro=ENABLE_GYRO_ALL_AXIS,
+                 acc_interrupt=False,
+                 gyro_interrupt=False,
+                 acc_scale=ACC_SCALE_16G,
+                 gyro_scale=GYRO_SCALE_2000DPS)
 def read_angle():
 
     while True:
-        acc_x, acc_y, acc_z, gx, gy, gz = sensor.get_readings()
 
-        print(acc_x, acc_y)
+        print(sensor.getAccData())
         time.sleep(0.2)
 
 read_angle()
