0230b2f822300ba00f0eb4909e26a43104b3b12b
Peli554 <pelayotcalle@gmail.com>
2025-03-31 15:32:17 +0100

    resetPoison() spam writes + resetPoison() on connection

--- SDPApp/app/src/main/java/com/example/sdpapp/bt/RangerBluetoothService.kt
+++ SDPApp/app/src/main/java/com/example/sdpapp/bt/RangerBluetoothService.kt
@@ -208,7 +208,7 @@ class RangerBluetoothService : Service() {
                     if (c.uuid.toString() == POISON_STATE_UUID) {
                         Log.i(TAG, "Found poison state characteristic")
                         poisonStateChar = c
-                        setPoisonCharacteristicNotification(c, true) // Enable notifications
+                        setPoisonCharacteristicIndication(c, true) // Enable notifications
                     }
                     if (c.uuid.toString() == RESET_POISON_UUID) {
                         Log.i(TAG, "Found reset poison characteristic")
@@ -389,6 +389,7 @@ class RangerBluetoothService : Service() {
                 val connected = bg.connect()
                 Log.d(TAG, "Connection status: $connected")
                 bluetoothGatt = bg
+                resetPoison()
                 return connected
             } catch (exception: IllegalArgumentException) {
                 Log.e(TAG, "Device not found with the provided address $address")
@@ -418,8 +419,11 @@ class RangerBluetoothService : Service() {
             Log.e(TAG, "No BLUETOOTH_CONNECT permission to reset poison")
             return
         }
-
         ch.value = byteArrayOf(0x00)
+        for (writes in 0..10) {
+            gatt.writeCharacteristic(ch)
+        }
+        //ch.value = byteArrayOf(0x00)
         val success = gatt.writeCharacteristic(ch)
         if (success) {
             Log.i(TAG, "Poison reset sent ✅")
@@ -445,7 +449,7 @@ class RangerBluetoothService : Service() {
         const val NOTIFICATION_ID = 1
     }
 
-    private fun setPoisonCharacteristicNotification(characteristic: BluetoothGattCharacteristic, enabled: Boolean) {
+    private fun setPoisonCharacteristicIndication(characteristic: BluetoothGattCharacteristic, enabled: Boolean) {
         val gatt = bluetoothGatt
         if (gatt == null) {
             Log.e(TAG, "BluetoothGatt not initialized")
