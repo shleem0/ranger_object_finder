c78213fd0b9913fa8378b93b26777b030c14ff2d
shleem0 <sholtokinghorn@gmail.com>
2025-03-31 15:27:16 +0100

    qwergrhnf

--- ranger_nav/imu.py
+++ ranger_nav/imu.py
@@ -1,19 +1,13 @@
-from LSM6DS3 import *
+from lsm6ds3 import LSM6DS3
 import time
 
-sensor = LSM6DS3(ACC_ODR=ACC_ODR_1_66_KHz,
-                 GYRO_ODR=GYRO_ODR_1_66_KHS,
-                 enable_acc=ENABLE_ACC_ALL_AXIS,
-                 enable_gyro=ENABLE_GYRO_ALL_AXIS,
-                 acc_interrupt=False,
-                 gyro_interrupt=False,
-                 acc_scale=ACC_SCALE_16G,
-                 gyro_scale=GYRO_SCALE_2000DPS)
+sensor = LSM6DS3()
 def read_angle():
 
     while True:
 
-        print(sensor.getAccData())
+        ax, ay, az, gx, gy, gz = sensor.get_readings()
+        print(ax, ay)
         time.sleep(0.2)
 
 read_angle()
