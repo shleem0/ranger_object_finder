4cf4e00e4c41339b3885b054d2e48c65cb58b4a7
shleem0 <sholtokinghorn@gmail.com>
2025-03-31 15:01:20 +0100

    imu

--- ranger_nav/imu.py
+++ ranger_nav/imu.py
@@ -1,14 +1,14 @@
+from machine import I2C
+import lsm6ds3 import LSM6DS3, NORMAL_MODE_104HZ
 import time
-import adafruit_lsm303_accel
-import board
 
-i2c = board.I2C()
-sensor = adafruit_lsm303_accel.LSM303_Accel(i2c)
+i2c = I2C(0, scl=13, sda=12)
+sensor = LSM6DS3(i2c, mode=NORMAL_MODE_104HZ)
 
 def read_angle():
 
     while True:
-        acc_x, acc_y = sensor.acceleration
+        acc_x, acc_y, acc_z, gx, gy, gz = sensor.get_readings()
 
         print(acc_x, acc_y)
         time.sleep(0.2)
