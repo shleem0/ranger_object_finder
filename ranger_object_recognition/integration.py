739b728f70b783a2f4926ae0efb4a9f9669c54a6
Kian3 <Kian@kiandehmahdi.co.uk>
2025-04-01 21:43:16 +0100

    silly

--- ranger_object_recognition/integration.py
+++ ranger_object_recognition/integration.py
@@ -138,12 +138,10 @@ def find_item_in_scene(scene_path, visualise = False):
             center_y = (box[1] + box[3]) / 2.0
             # Convert x and y pixel coordinates to cm using your measurements functions:
             distance_x_cm = measurements.x_pixel_to_cm(center_x, full_width)
-            distance_y_cm = measurements.pixels_to_cm(center_y, full_height)
+            distance_y_cm = measurements.pixels_to_cm(center_y, full_height) - 5
             print("Detected coordinates x = {}, y = {}".format(distance_y_cm, distance_x_cm))
-            if distance_y_cm > 0:
-                distance_y_cm -= 5
-            else:
-                distance_y_cm += 15
+            if distance_x_cm < 0:
+                distance_x_cm += 5
             # print(f"Crop {idx} center is at (X: {distance_x_cm:.2f} cm, Y: {distance_y_cm:.2f} cm) from camera.", file=sys.stderr)
             # send_coordinates(distance_x_cm, distance_y_cm) 
             send_coordinates(distance_y_cm, distance_x_cm) # Swapped x and y for arduino
