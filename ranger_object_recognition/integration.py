e22752fdb122a2e82ad31f2d0fd4eaced5e49e96
Kian3 <Kian@kiandehmahdi.co.uk>
2025-04-01 22:08:05 +0100

    Update integration.py

--- ranger_object_recognition/integration.py
+++ ranger_object_recognition/integration.py
@@ -141,7 +141,7 @@ def find_item_in_scene(scene_path, visualise = False):
             distance_y_cm = measurements.pixels_to_cm(center_y, full_height) - 5
             print("Detected coordinates x = {}, y = {}".format(distance_y_cm, distance_x_cm))
             if distance_x_cm < 0:
-                distance_x_cm += 6
+                distance_x_cm += 4
             # print(f"Crop {idx} center is at (X: {distance_x_cm:.2f} cm, Y: {distance_y_cm:.2f} cm) from camera.", file=sys.stderr)
             # send_coordinates(distance_x_cm, distance_y_cm) 
             send_coordinates(distance_y_cm, distance_x_cm) # Swapped x and y for arduino
